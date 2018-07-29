f37ceb5767055dbfa0f5fa41c7f1e4bba3d9e98b
geraldokutar <geraldpilk@gmail.com>
2018-07-29 21:53:25 +0300

    change the content in the append brackets

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
     var exclamationInput = $("input#exclamation").val();
     var varbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+    $(".person1").append("person1Input");
+    $(".person2").append("person2Input");
+    $(".animal").append("animalInput");
+    $(".exclamation").append("exclamationInput");
+    $(".verb").append("verbInput");
+    $(".noun").append("nounInput");
 
     $("#story").show();
 
